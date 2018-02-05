aec610637485a2c65e25c109e206d00becfaef43
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2018-02-04 16:47:59 -0800

    Added about page

--- src/main/java/com/devopsbuddy2/web/controllers/CopyController.java
+++ src/main/java/com/devopsbuddy2/web/controllers/CopyController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class CopyController {
 
-    @RequestMapping ("/")
-    public String home(){
-        return "index";
+    @RequestMapping("/about")
+    public String about(){
+        return "copy/about";
     }
-
 }
