bcd6be1526013e01a568cc9f9591894517c1eba1
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2018-02-05 22:03:32 -0800

    Created the JPA entity model for Spring Security

---
+++ src/main/java/com/devopsbuddy2/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,59 @@
+package com.devopsbuddy2.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Role implements Serializable {
+    /** The Serial Version UID for Serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public Role(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+
+}
